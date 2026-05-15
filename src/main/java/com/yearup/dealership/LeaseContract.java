85cb01fcccc3064b6c065f73d9178aed4e8036f4
tk_mucci <takudzwamucherengi@gmail.com>
2026-05-15 16:25:55 -0500

    7th commit: updated my LeaseContract class to getMonthlyPayment method with the correct logic.

--- src/main/java/com/yearup/dealership/LeaseContract.java
+++ src/main/java/com/yearup/dealership/LeaseContract.java
@@ -8,16 +8,28 @@ public class LeaseContract extends Contract{
     public LeaseContract(String dateOfContract, String customerName, String customerEmail, Vehicle vehicleSold){
 
         super(dateOfContract, customerName, customerEmail, vehicleSold);
+
+        this.expectedEndingValue = vehicleSold.getPrice() * 0.5;
+        this.leaseFee = vehicleSold.getPrice() * 0.07;
+
     }
 
     @Override
     public double getTotalPrice() {
-        return getTotalPrice() + leaseFee;
+        return expectedEndingValue + leaseFee;
     }
 
     @Override
     public double getMonthlyPayment() {
-        return (totalPrice * 0.04) + getMonthlyPayment();
+
+        double principal = getTotalPrice();
+        double annualRate = 0.04;
+        int months = 36;
+
+        double monthlyRate = annualRate / 12;
+        double top = monthlyRate * Math.pow(1 + monthlyRate, months);
+        double bottom = Math.pow(1 + monthlyRate, months) - 1;
+        return ((principal * top) / bottom)  ;
     }
 
     public double getExpectedEndingValue() {
